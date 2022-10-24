a86b0b84010a198b0cbb33bc3d250840dd60287f
hoon2 <sanghoon.engineer@gmail.com>
2022-10-25 08:01:47 +0900

    과제 초기 모습

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import GlobalStyle from "./GlobalStyle";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SideBar = styled.div`
+  width: 200px;
+  height: 600px;
+  background-color: antiquewhite;
+`;
+
+const Footer = styled.footer`
+  width: 800px;
+  height: 100px;
+  background-color: teal;
+`;
+
+const Main = styled.main`
+  width: 600px;
+  height: 600px;
+  background-color: purple;
+`;
+
+const Navigator = styled.div`
+  height: 100px;
+  width: 800px;
+  background-color: aquamarine;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>안녕</div>
+      <Container>
+        <Navigator>네비게이터</Navigator>
+        <SideBar>사이드바</SideBar>
+        <Main>메인</Main>
+        <Footer>푸터</Footer>
+      </Container>
     </>
   );
 }
